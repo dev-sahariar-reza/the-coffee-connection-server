49ee5b69a8143faf62e9db9de18911ce9f0ce793
dev-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-10-23 21:12:32 +0600

    get one coffee data operation done

--- index.js
+++ index.js
@@ -42,8 +42,17 @@ async function run() {
     });
 
     // get one coffee data
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
 
     // update one coffee data
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+    });
 
     // delete coffee data
     app.delete("/coffee/:id", async (req, res) => {
